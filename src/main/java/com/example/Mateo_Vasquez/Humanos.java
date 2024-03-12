6830543a0d1b23d3b3eeb049e9eddeac4850f01a
Lanza43 <mateo.vasquezg@udea.edu.co>
2024-03-12 12:22:54 -0500

    second commit

---
+++ src/main/java/com/example/Mateo_Vasquez/Humanos.java
@@ -0,0 +1,8 @@
+package com.example.Mateo_Vasquez;
+
+public class Humanos {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, puede que ya me conozcan o puede que no, pero mucho gusto");
+        System.out.println("Mi nombre es Mateo, ademas ¡¡HOLA!!");
+    }
+}
